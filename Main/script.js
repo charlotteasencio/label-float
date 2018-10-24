67b552d84b8869935018a1e3303e5d5e14a2e604
charlotteasencio <charlotte.e.asencio@gmail.com>
2018-10-23 22:11:42 -0400

    remove extra files

--- Main/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-   $(".text-input input").change(function(){
-       if( $("this").val() != "") {
-           $("this").addClass("filled")
-       } else {
-           $("this").removeClass('filled')
-       }
-   })
-});
